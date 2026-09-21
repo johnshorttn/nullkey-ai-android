35c214d296aadbb6f2ca644f6baa5749b75841b8
johnshorttn <john.short.tn@gmail.com>
2026-09-20 22:35:46 -0400

    test: run Android JSON backup tests with Robolectric

--- app/src/test/java/com/nullverse/nullkeyai/clipboard/ClipBackupSecurityTest.kt
+++ app/src/test/java/com/nullverse/nullkeyai/clipboard/ClipBackupSecurityTest.kt
@@ -5,7 +5,10 @@ import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertThrows
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
 
+@RunWith(RobolectricTestRunner::class)
 class ClipBackupSecurityTest {
     @Test
     fun unprotectedJsonRoundTripRemainsImportable() {
