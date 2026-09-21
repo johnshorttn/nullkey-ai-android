189bba7ba522235041fe4a9bc09727c82815769d
johnshorttn <john.short.tn@gmail.com>
2026-09-20 22:36:50 -0400

    test: pin backup JSON tests to Android 34 runtime

--- app/src/test/java/com/nullverse/nullkeyai/clipboard/ClipBackupSecurityTest.kt
+++ app/src/test/java/com/nullverse/nullkeyai/clipboard/ClipBackupSecurityTest.kt
@@ -7,8 +7,10 @@ import org.junit.Assert.assertThrows
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
+@Config(sdk = [34])
 class ClipBackupSecurityTest {
     @Test
     fun unprotectedJsonRoundTripRemainsImportable() {
